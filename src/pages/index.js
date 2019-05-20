379ebc10652980ceea10f65f940fd613fd5e95fa
EricHeredia <EricDavidHeredia@gmail.com>
2019-05-20 02:14:16 -0500

    clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Eric, a full-stack developer living in beautiful Chicago, IL.</h2>
-      <p>Need a developer? <Link to="/contact">Contact me</Link>.</p>
-      <p>More information <Link to="/about">About me</Link>.</p>
     </Layout>
   )
 }
