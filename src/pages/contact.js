03bad79643850a129b512e6f656c8021e2ece067
EricHeredia <EricDavidHeredia@gmail.com>
2019-05-20 21:20:30 -0500

    Changed contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactMe = () => {
     <Layout>
       <Head title="Contact" />
       <h1>Contact Me</h1>
-      <p>Find full contact details on my <a href="https://github.com/EricHeredia" target="_blank" rel="noopener noreferrer">GitHub</a> profile!</p>
+      <p>E-Mail and portfolio page can be found on my <a href="https://github.com/EricHeredia" target="_blank" rel="noopener noreferrer">GitHub</a> profile!</p>
     </Layout>
   )
 }
